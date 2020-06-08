5288d2a7e5ff2e9d77230deff5043728138e6f5a
Dan Burtenshaw <dan.68@hotmail.co.uk>
2020-06-08 10:51:56 +0100

    Test

--- src/actions/prUpdatedAction.ts
+++ src/actions/prUpdatedAction.ts
@@ -1,15 +1,16 @@
 import * as github from "@actions/github";
-import S3 from '../s3Client';
-import s3UploadDirectory from '../utils/s3UploadDirectory';
-import validateEnvVars from '../utils/validateEnvVars';
-import checkBucketExists from '../utils/checkBucketExists';
-import githubClient from '../githubClient';
+import S3 from "../s3Client";
+import s3UploadDirectory from "../utils/s3UploadDirectory";
+import validateEnvVars from "../utils/validateEnvVars";
+import checkBucketExists from "../utils/checkBucketExists";
+import githubClient from "../githubClient";
 import deactivateDeployments from "../utils/deactivateDeployments";
+import { ReposCreateDeploymentResponseData } from "@octokit/types";
 
 export const requiredEnvVars = [
-  'AWS_ACCESS_KEY_ID',
-  'AWS_SECRET_ACCESS_KEY',
-  'GITHUB_TOKEN'
+  "AWS_ACCESS_KEY_ID",
+  "AWS_SECRET_ACCESS_KEY",
+  "GITHUB_TOKEN",
 ];
 
 export default async (bucketName: string, uploadDirectory: string) => {
@@ -17,29 +18,29 @@ export default async (bucketName: string, uploadDirectory: string) => {
   const { repo } = github.context;
   const branchName = github.context.payload.pull_request!.head.ref;
 
-  console.log('PR Updated');
+  console.log("PR Updated");
 
   validateEnvVars(requiredEnvVars);
 
   const bucketExists = await checkBucketExists(bucketName);
 
   if (!bucketExists) {
-    console.log('S3 bucket does not exist. Creating...');
+    console.log("S3 bucket does not exist. Creating...");
     await S3.createBucket({ Bucket: bucketName }).promise();
 
-    console.log('Configuring bucket website...');
+    console.log("Configuring bucket website...");
     await S3.putBucketWebsite({
       Bucket: bucketName,
       WebsiteConfiguration: {
-        IndexDocument: { Suffix: 'index.html' },
-        ErrorDocument: { Key: 'index.html' }
-      }
+        IndexDocument: { Suffix: "index.html" },
+        ErrorDocument: { Key: "index.html" },
+      },
     }).promise();
   } else {
-    console.log('S3 Bucket already exists. Skipping creation...');
+    console.log("S3 Bucket already exists. Skipping creation...");
   }
 
-  await deactivateDeployments(repo)
+  await deactivateDeployments(repo);
 
   const deployment = await githubClient.repos.createDeployment({
     ...repo,
@@ -47,24 +48,32 @@ export default async (bucketName: string, uploadDirectory: string) => {
     environment: `PR-${github.context.payload.pull_request!.number}`,
     auto_merge: false,
     transient_environment: true,
-    required_contexts: []
+    required_contexts: [],
   });
 
-  await githubClient.repos.createDeploymentStatus({
-    ...repo,
-    deployment_id: deployment.data.id,
-    state: 'in_progress',
-  });
+  if (isSuccessResponse(deployment.data)) {
+    await githubClient.repos.createDeploymentStatus({
+      ...repo,
+      deployment_id: deployment.data.id,
+      state: "in_progress",
+    });
 
-  console.log('Uploading files...');
-  await s3UploadDirectory(bucketName, uploadDirectory);
+    console.log("Uploading files...");
+    await s3UploadDirectory(bucketName, uploadDirectory);
 
-  await githubClient.repos.createDeploymentStatus({
-    ...repo,
-    deployment_id: deployment.data.id,
-    state: 'success',
-    environment_url: websiteUrl
-  });
+    await githubClient.repos.createDeploymentStatus({
+      ...repo,
+      deployment_id: deployment.data.id,
+      state: "success",
+      environment_url: websiteUrl,
+    });
 
-  console.log(`Website URL: ${websiteUrl}`);
+    console.log(`Website URL: ${websiteUrl}`);
+  }
 };
+
+function isSuccessResponse(
+  object: any
+): object is ReposCreateDeploymentResponseData {
+  return "id" in object;
+}
